e2e86a5c440d205688c40601ed2d1321cb14dcb9
Ankit Modi <ankumodi1709@gmail.com>
2025-05-16 11:37:59 +0530

    user updation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,35 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [ :edit, :update]
   def new
     @user = User.new
+  end
+
+  def edit
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
       flash[:notice] = "Welcome to Alpha blog, you have succesfully signed up."
-      redirect_to @user
+      redirect_to articles_path
     else
       render :new
     end
   end
+  
+  def update
+    if @user.update(user_params)
+      flash[:notice] = "Your account info was successfully updated."
+      redirect_to articles_path
+    else
+      render :edit
+    end
+  end
 
   private
-  #def set_article
-  #  @article = Article.find(params[:id])
-  #end
+  def set_user
+    @user = User.find(params[:id])
+  end
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
