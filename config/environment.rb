5cf71cdf824e1f934e46e99f249f1023fa21ed6f
Ankit Modi <ankumodi1709@gmail.com>
2025-05-20 15:16:37 +0530

    header and login logout with helpers

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+Rails.application.config.action_view.field_error_proc = Proc.new do |html_tag, instance|
+  if html_tag =~ /<input|textarea|select/
+    doc = Nokogiri::HTML::DocumentFragment.parse(html_tag)
+    tag = doc.children[0]
+    tag['class'] = "#{tag['class']} is-invalid".strip
+    doc.to_html.html_safe
+  else
+    html_tag.html_safe
+  end
+end
