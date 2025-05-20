9240b4a883bf0684659a5db32de8f650ede7b678
Ankit Modi <ankumodi1709@gmail.com>
2025-05-20 18:57:20 +0530

    Delete user account with associated articles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [ :edit, :update, :show]
+  before_action :set_user, only: [ :edit, :update, :show, :destroy]
+  before_action :require_user, only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
+
+
   def new
     @user = User.new
   end
@@ -35,6 +39,12 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    @user.destroy
+    session[:user_id] = nil
+    flash[:notice] = "Account and all associated articles successfully deleted"
+    redirect_to articles_path
+  end
   private
   def set_user
     @user = User.find(params[:id])
@@ -44,4 +54,10 @@ class UsersController < ApplicationController
     params.require(:user).permit(:username, :email, :password)
   end
 
+  def require_same_user
+    if current_user != @user
+      flash[:alert] = "You can only edit your own account"
+      redirect_to @user
+    end
+  end
 end
